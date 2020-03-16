b34c1dc4e3bb660e8f3837d79be5d1f33e1d2fc6
a-yashiro <a_yasiro2000@ybb.ne.jp>
2020-03-16 23:54:30 +0900

    各種タグからCP932入りのものとタグが存在しないものを検索

--- ModifyMusicTagUTF.py
+++ ModifyMusicTagUTF.py
@@ -3,9 +3,12 @@ import os
 from pathlib import Path
 import eyed3
 import chardet
+import inspect
 
 #global変数
-
+logConvertFile = Path()
+logErrorFile = Path()
+logNoTagFile = Path()
 
 #フォルダ単位で変換処理実行
 def ExecTagCheck(outLogPath,inFolder):
@@ -14,31 +17,39 @@ def ExecTagCheck(outLogPath,inFolder):
 	#ファイルの解析
 	files_list = inFolder.glob("*.mp3")
 	for count,file in enumerate(files_list):
-		print (" checking file ["+"{:0=3}".format(count)+"] " + file.name)
+		print (" checking file ["+"{:0=3}".format(count)+"] " + str(file.resolve()))
 
 		#タグ解析
-		#tags = ID3(file)
+		#Invalid UFIDのエラー時々出て邪魔
 		audiofile = eyed3.load(file)
 		tags = audiofile.tag
+
+		is_first_tag = True
+		for member, value in inspect.getmembers(tags):
+			is_utf = True
+			#print(str(member+":"+str(value)))
+			try:
+				# 判定文字コードがasciiだったら何もしない、utf8は怪しいのでLatin>CP932の変換で例外チェック
+				if chardet.detect(bytes(value,"utf-8"))["encoding"] != "ascii":
+					if bytes(value,"latin1").decode("cp932"):
+						is_utf = False
+			except:
+				is_utf = True	#Latin1に変換できなかったならUTF8とみなしてそのまま
+			if is_utf != True:
+				print("find cp932:"+str(member)+"["+str(value)+"]")
+				print(str(member) + ":" +bytes(value,"latin1").decode("cp932"))
+
+				#ログを出力
+				if is_first_tag:
+					logConvertFile.write(str(file.resolve())+"\n")
+					is_first_tag = False
+				logConvertFile.write("\t"+str(member) + ":" +bytes(value,"latin1").decode("cp932")+"\n")
+
 		# そもそもタグが入っていないケース
 		if tags is None or tags.title is None:
-			continue
+			logNoTagFile.write(str(file.resolve())+"\n")
+			#logNoTagFile.write("\t"+str(member) + ":" +bytes(value,"latin1").decode("cp932")+"\n")
 
-		print(tags.title)
-		# utf8だったらそのまま
-		is_utf = True
-		try:
-			# 判定文字コードがasciiだったら何もしない、utf8は怪しいのでLatin>CP932の変換で例外チェック
-			if chardet.detect(bytes(tags.title,"utf-8"))["encoding"] != "ascii":
-				if bytes(tags.title,"latin1").decode("cp932"):
-					is_utf = False
-		except:
-			is_utf = True	#Latin1に変換できなかったならUTF8とみなしてそのまま
-		if is_utf != True:
-			print(chardet.detect(bytes(tags.title,"latin1")))
-			print(bytes(tags.title,"latin1").decode("cp932"))
-		else:
-			print("skip")
 		print("***********************")
 
 
@@ -54,7 +65,25 @@ if __name__ == "__main__":
 		if not outLogPath.exists():
 			outLogPath.mkdir()
 
+		#出力フォルダの遅延生成を完了待ち
+		while True:
+			if outLogPath.exists():
+				break
+
+		#ログファイル作成
+		
+		logConvertFile = (outLogPath / "logConvertFile.txt").open(mode='w')
+		logConvertFile.write("cp932 tag convert file list\n\n")
+		logErrorFile = (outLogPath / "logErrorFile.txt").open(mode='w')
+		logErrorFile.write("convert error file list\n\n")
+		logNoTagFile = (outLogPath / "logNoTagFiles.txt").open(mode='w')
+		logNoTagFile.write("no tag added file list\n\n")
+
 		for count,arg in enumerate(args):
 			if count > 1:
 				if os.path.exists(arg):
 					ExecTagCheck(outLogPath,Path(arg))
+
+		logConvertFile.close()
+		logErrorFile.close()
+		logNoTagFile.close()
