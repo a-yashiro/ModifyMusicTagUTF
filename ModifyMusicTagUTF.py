0576e783a6ee50926a3e76766ccdd7863327ac59
a-yashiro <a_yasiro2000@ybb.ne.jp>
2020-04-11 00:23:35 +0900

    NoTagケース、親フォルダからタグをつくるパターン追加

--- ModifyMusicTagUTF.py
+++ ModifyMusicTagUTF.py
@@ -10,18 +10,25 @@ import re
 logConvertFile = Path()
 logErrorFile = Path()
 logNoTagFile = Path()
+logNoTagAlbumFile = Path()
+logNoTagUnknownFile = Path()
 logSkipFile = Path()
+
+cMinStringLengthAlbumPath = 10
 
 re_title_from_filename = re.compile('^\\s*\\(.*\\)\\s*(.+)\\.mp3')
 re_artist_from_filename = re.compile('^\\s*\\((.*)\\)\\s*.+\\.mp3')
+re_title_from_albam_filename = re.compile('^\\s*[0-9]+\\s*[_]*(.+)\\.mp3')
 
 #そもそもID3タグがはいってないので、ファイル名からタグを作る
 def CreateID3TagsFromFileName(inFile,inAudioFile,isCheckOnly):
 	filename = inFile.name
-	# ファイル名が (***)***.mp3の形式の場合のみ
+	# ファイル名が (***)***.mp3の形式の場合
 	if re_title_from_filename.match ( filename ):
 		title = re_title_from_filename.sub('\\1', filename)
 		artist = re_artist_from_filename.sub('\\1',filename)
+		
+		logNoTagFile.write(str(inFile.resolve())+"\n")
 		logNoTagFile.write("\tArtist:"+artist + "\n\tTitle:" + title +"\n")
 		
 		#変換したタグを保存
@@ -34,12 +41,46 @@ def CreateID3TagsFromFileName(inFile,inAudioFile,isCheckOnly):
 			tags.save(encoding='utf-16', version=(2, 3, 0))
 			
 		return True
+	#ファイル名が数字****.mp3の場合は、Albamだと思われるので、親フォルダをalbumにセットする
+	elif re_title_from_albam_filename.match ( filename ):
+		title = re_title_from_albam_filename.sub('\\1', filename)
+		album = inFile.parent.name
+
+		logNoTagAlbumFile.write(str(inFile.resolve())+"\n")
+		logNoTagAlbumFile.write("\talbum:"+album + "\n\tTitle:" + title +"\n")
+		
+		#変換したタグを保存
+		if not isCheckOnly:
+			inAudioFile.initTag()
+			tags = inAudioFile.tag
+			tags.artist = album
+			tags.album = album
+			tags.title = title
+			tags.save(encoding='utf-16', version=(2, 3, 0))
+			
+		return True
+	# 親フォルダ名が10byte以上あったらそれがalbum or artist名とみなす
+	elif len ( bytes (inFile.parent.name, 'utf8' )) >= 10:
+		title = re_title_from_albam_filename.sub('\\1', filename)
+		album = inFile.parent.name
+
+		logNoTagUnknownFile.write(str(inFile.resolve())+"\n")
+		logNoTagUnknownFile.write("\talbum:"+album + "\n\tTitle:" + title +"\n")
+		
+		#変換したタグを保存
+		if not isCheckOnly:
+			inAudioFile.initTag()
+			tags = inAudioFile.tag
+			tags.artist = album
+			tags.album = album
+			tags.title = title
+			tags.save(encoding='utf-16', version=(2, 3, 0))
+			
+		return True
 	else:
 		logErrorFile.write(str(inFile.resolve())+"\n")
 		logErrorFile.write("\tno ID3 tag but wrong file name pattern" +"\n")
 		return False
-
-
 
 #フォルダ単位で変換処理実行
 def ExecTagCheck(outLogPath,inFolder,isCheckOnly):
@@ -50,6 +91,7 @@ def ExecTagCheck(outLogPath,inFolder,isCheckOnly):
 	# 注意、files_listは配列ではなくGenerator
 
 	for count,file in enumerate(files_list):
+		#ログに書き込むときに暗黙のエンコード変換に失敗すると止まるのでIgnoreつけて変換しておく
 		print (" checking file ["+"{:0=3}".format(count)+"] " + str(file.resolve()))
 
 		#タグ解析
@@ -89,17 +131,17 @@ def ExecTagCheck(outLogPath,inFolder,isCheckOnly):
 						is_first_tag = False
 					logConvertFile.write("\t"+str(member) + ":" +bytes(value,"latin1").decode("cp932")+"\n")
 
-				except:
+				except Exception as e:
+					# Ascii+Latin1の場合ここに来る可能性あり
 					logErrorFile.write(str(file.resolve())+"\n")
 					logErrorFile.write("\tno converted utf id3 tag save error" +"\n")
+					logErrorFile.write("\t" + str(e) +"\n")
 
 		# そもそもタグが入っていないケース
 		if tags is None or tags.title is None:
 			#logNoTagFile.write("\t"+str(member) + ":" +bytes(value,"latin1").decode("cp932")+"\n")
 			is_skipped = False
-			res = CreateID3TagsFromFileName(file,audiofile, isCheckOnly)
-			if res:
-				logNoTagFile.write(str(file.resolve())+"\n")
+			CreateID3TagsFromFileName(file,audiofile, isCheckOnly)
 
 		if is_skipped:
 			logSkipFile.write(str(file.resolve())+"\n")
@@ -127,13 +169,17 @@ if __name__ == "__main__":
 
 		#ログファイル作成
 		
-		logConvertFile = (outLogPath / "logConvertFile.txt").open(mode='w')
+		logConvertFile = (outLogPath / "logConvertFile.txt").open(mode='w', encoding='UTF-8')
 		logConvertFile.write("cp932 tag convert file list\n\n")
-		logErrorFile = (outLogPath / "logErrorFile.txt").open(mode='w')
+		logErrorFile = (outLogPath / "logErrorFile.txt").open(mode='w', encoding='UTF-8')
 		logErrorFile.write("convert error file list\n\n")
-		logNoTagFile = (outLogPath / "logNoTagFiles.txt").open(mode='w')
+		logNoTagFile = (outLogPath / "logNoTagFiles.txt").open(mode='w', encoding='UTF-8')
 		logNoTagFile.write("no tag added file list\n\n")
-		logSkipFile = (outLogPath / "logSkipFile.txt").open(mode='w')
+		logNoTagAlbumFile = (outLogPath / "logNoTagAlbumFile.txt").open(mode='w', encoding='UTF-8')
+		logNoTagAlbumFile.write("no tag added file for album folder list\n\n")
+		logNoTagUnknownFile = (outLogPath / "logNoTagUnknownFile.txt").open(mode='w', encoding='UTF-8')
+		logNoTagUnknownFile.write("no tag added file unknown folder list\n\n")
+		logSkipFile = (outLogPath / "logSkipFile.txt").open(mode='w', encoding='UTF-8')
 		logSkipFile.write("skip file list\n\n")
 
 		isCheckOnly = False
